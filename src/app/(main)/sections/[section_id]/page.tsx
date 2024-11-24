476d15fdbb78a9b07c809fc36faec0f6d99a0094
ysung327 <ysung327@naver.com>
2024-11-24 23:20:52 +0900

    fix build error

--- src/app/(main)/sections/[section_id]/page.tsx
+++ src/app/(main)/sections/[section_id]/page.tsx
@@ -1,9 +1,9 @@
 // [homepage_id] is a dynamic route
 
-export default function Page({ section_id }: { section_id: string }) {
+export default function Page({ params }: { params: { section_id: string } }) {
   return (
     <main className="min-h-screen w-full flex flex-col items-center justify-between">
-      섹션 레퍼런스의 디테일 페이지입니다. ({section_id})
+      섹션 레퍼런스의 디테일 페이지입니다. ({params.section_id})
     </main>
   );
 }

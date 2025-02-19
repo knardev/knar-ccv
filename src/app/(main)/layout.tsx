e835a34b315aa82b12510faee4ca1705c5605048
ysung327 <ysung327@naver.com>
2025-02-20 00:01:02 +0900

    edit authentication flow

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -7,10 +7,12 @@ import { Providers } from "@/components/provider";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
 import { TopNavigationMenu } from "@/components/layout/top-navigation-menu";
+import { Button } from "@/components/ui/button";
 // actions
 import { checkAccountRole } from "@/utils/auth/check-account-role";
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
+import Link from "next/link";
 
 const geistSans = localFont({
   src: "../fonts/GeistVF.woff",
@@ -41,18 +43,40 @@ export default async function RootLayout({
   } = await supabase.auth.getUser();
   const accountRole = await checkAccountRole();
 
-  if (!user) {
-    redirect("/login");
-  }
+  // if (!user) {
+  //   redirect("/login");
+  // }
 
-  if (accountRole === "UNAUTHORIZED") {
+  if (accountRole === "UNAUTHORIZED" || !user) {
     return (
-      <div className="flex justify-center items-center h-screen">
-        <div className="text-center">
-          <h1 className="text-4xl font-bold mb-4">보기 권한이 없습니다.</h1>
-          <p className="text-lg">관리자에게 요청해주세요.</p>
-        </div>
-      </div>
+      <html lang="ko">
+        <Providers>
+          <body
+            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          >
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange
+            >
+              <div className="flex justify-center items-center h-screen">
+                <div className="text-center">
+                  <h1 className="text-4xl font-bold mb-4">
+                    보기 권한이 없습니다.
+                  </h1>
+                  <p className="text-lg mb-4">관리자에게 요청해주세요.</p>
+                  {!user && (
+                    <Button asChild variant="outline">
+                      <Link href="/login">로그인</Link>
+                    </Button>
+                  )}
+                </div>
+              </div>
+            </ThemeProvider>
+          </body>
+        </Providers>
+      </html>
     );
   }
   return (
